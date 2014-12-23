f8f25430a059fa784ad3aa02b86110d158144d58
manishkk74@gmail.com <manishkk74@gmail.com@d6a45ad9-688f-1c7c-f15b-68b1343bae16>
2014-12-23 05:41:23 +0000

    add

---
+++ configs/config.localhost_8888.php
@@ -0,0 +1,5 @@
+<?php
+define('CLIENTID', '437724595536-5ra6e0p4lfqre9m9q70cu0knpfbkcr1m.apps.googleusercontent.com');
+define('CLIENTSECRET', 'QQ8pvbdx3Fc3BoRI6pDZupEE');
+define('DEVELOPERKEY', 'AIzaSyBvXqWIcqyTVRgjXsVjDbdORcNaXHVjtOw');
+?>
---
+++ users/configs.php
@@ -0,0 +1,5 @@
+<?php
+require_once SITEDIR.'/api/googleauth/src/Google_Client.php'; // include the required calss files for google login
+require_once SITEDIR.'/api/googleauth/src/contrib/Google_PlusService.php';
+require_once SITEDIR.'/api/googleauth/src/contrib/Google_Oauth2Service.php';
+?>
